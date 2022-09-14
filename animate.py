a5ac85f9a195b85c9289079df9080322552420f7
gtoombs-microsaas <greg.toombs@microsaas.co.uk>
2022-09-13 20:17:45 -0400

    Animation forward, better parity

--- animate.py
+++ animate.py
@@ -1,204 +1,65 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3 -OO
 import sys
 from bisect import bisect
 from io import StringIO
-from itertools import chain
-from math import sqrt
+from itertools import chain, islice
 from pathlib import Path
 from sys import stdin, stdout
-from typing import Iterator, Optional, NamedTuple, Sequence, TextIO, Iterable
+from typing import Iterator, Optional, TextIO
 
 import tkinter as tk
 
-
-NDEBUG = True
-
-DELAY = 10        # seconds
-SPEED = 2         # metres per second
-EDGE = 100        # metres
-
-# These are theoretical bounds; we get narrower than this
-DISTANCE_MIN = 1
-DISTANCE_MAX = sqrt(2) * EDGE
-TIME_MIN = DISTANCE_MIN / SPEED
-TIME_MAX = DISTANCE_MAX / SPEED
-
-
-def time_to(dx: int, dy: int) -> float:
-    if not NDEBUG:
-        assert -EDGE <= dx <= EDGE
-        assert -EDGE <= dy <= EDGE
-
-    return sqrt(dx**2 + dy**2) / SPEED
-
-
-def coord_min(x: int) -> int:
-    return max(1, min(EDGE - x, x))
-
-
-def coord_max(x: int) -> int:
-    return max(EDGE - x, x)
-
-
-class Waypoint(NamedTuple):
-    x: int
-    y: int
-    penalty: int = 0
-
-    def time_to(self, other: 'Waypoint') -> float:
-        return time_to(self.x - other.x, self.y - other.y)
-
-    @property
-    def time_min(self) -> float:
-        return time_to(coord_min(self.x), coord_min(self.y))
-
-    @property
-    def time_max(self) -> float:
-        return time_to(coord_max(self.x), coord_max(self.y))
-
-    def __str__(self) -> str:
-        return f'({self.x:3},{self.y:3}) p={self.penalty:3}'
-
-    @property
-    def is_sane(self) -> bool:
-        return (0 <= self.x <= EDGE and
-                0 <= self.y <= EDGE)
-
-
-class WaypointReader:
-    def __init__(self, in_: TextIO) -> None:
-        self.lines = in_.readlines()
-        self.pos = len(self.lines) - 1
-        self.case_count = 0
-        self.next_waypoint: Optional[Waypoint] = None
-        assert not self.advance_state()
-
-    @property
-    def stream_end(self) -> bool:
-        return self.pos < 0
-
-    def advance_state(self) -> bool:
-        if self.stream_end:
-            return False
-        line = self.lines[self.pos]
-        self.pos -= 1
-
-        fields = [int(f) for f in line.split()]
-
-        if len(fields) == 1:
-            count, = fields
-            if count == self.case_count:
-                self.case_count = 0
-                return False
-            raise ValueError()
-
-        self.next_waypoint = Waypoint(*fields)
-        self.case_count += 1
-        return True
-
-    def get_next(self) -> Waypoint:
-        return self.next_waypoint
-
-
-class OptimisedWaypoint(NamedTuple):
-    waypoint: Waypoint
-    best_cost: float
-    invariant_cost: float
-    cost_min: float
-    penalty: int
-
-    @classmethod
-    def with_cost(cls, waypoint: Waypoint, best_cost: float = 0) -> 'OptimisedWaypoint':
-        invariant_cost = best_cost - waypoint.penalty + DELAY
-        return cls(
-            waypoint=waypoint, best_cost=best_cost, invariant_cost=invariant_cost,
-            cost_min=waypoint.time_min + invariant_cost, penalty=waypoint.penalty,
-        )
-
-    def cost_from(self, visited: Waypoint) -> float:
-        time = visited.time_to(self.waypoint)
-        if not NDEBUG:
-            assert time <= TIME_MAX
-        return time + self.invariant_cost
-
-    @property
-    def cost_max(self) -> float:
-        return self.waypoint.time_max + self.invariant_cost
-
-    def emplace(self, into: list['OptimisedWaypoint']) -> bool:
-        i = bisect(a=into, x=self.cost_min, key=OptimisedWaypoint.sort_key)
-        into.insert(i, self)
-        return i == 0
-
-    def sort_key(self) -> float:
-        return self.invariant_cost
-
-    def __str__(self) -> str:
-        return f'{self.waypoint} ap={self.penalty:3} bt={self.best_cost:6.1f}'
-
-    @property
-    def is_sane(self) -> bool:
-        return self.waypoint.is_sane
+from main import (
+    EDGE, SPEED,
+    OptimisedWaypoint, Waypoint, time_to
+)
 
 
 class Solver:
-    HEAD = Waypoint(x=0, y=0)
-    TAIL = OptimisedWaypoint.with_cost(Waypoint(x=EDGE, y=EDGE))
+    HEAD = OptimisedWaypoint.with_cost(Waypoint(x=0, y=0))
+    TAIL = Waypoint(x=EDGE, y=EDGE)
 
     def __init__(self) -> None:
-        self.opt_waypoints = [self.TAIL]
+        self.opt_waypoints = [self.HEAD]
         self.acceptable_cost = float('inf')
         self.total_penalty = 0
-        self.waypoints: list[Waypoint] = [self.TAIL.waypoint]
-        self.best_cost: Optional[float] = None
+        self.waypoints: list[Waypoint] = [self.HEAD.waypoint]
+        self.cost_best: Optional[float] = None
         self.cost: Optional[float] = None
         self.prune_from: Optional[int] = None
         self.visited: Optional[Waypoint] = None
-        self.skipto: Optional[OptimisedWaypoint] = None
+        self.skip_from: Optional[OptimisedWaypoint] = None
         self.new_opt: Optional[OptimisedWaypoint] = None
         self.final_time: Optional[float] = None
 
     def prune(self) -> Iterator['Solver']:
-        # opt_waypoints must be in increasing order of invariant cost
         self.prune_from = bisect(a=self.opt_waypoints, x=self.acceptable_cost, key=OptimisedWaypoint.sort_key)
-        if not NDEBUG:
-            assert self.prune_from > 0
+        assert self.prune_from > 0
         while len(self.opt_waypoints) > self.prune_from:
             yield self
             self.opt_waypoints.pop()
         self.prune_from = None
 
     def get_best_cost(self) -> Iterator['Solver']:
-        self.best_cost = float('inf')
-        for self.skipto in self.opt_waypoints:
-            if not NDEBUG:
-                assert self.skipto.is_sane
-            self.cost = self.skipto.cost_from(self.visited)
-            self.best_cost = min(self.best_cost, self.cost)
+        self.cost_best = float('inf')
+        for self.skip_from in self.opt_waypoints:
+            self.cost = self.skip_from.cost_to(self.visited)
+            self.cost_best = min(self.cost_best, self.cost)
             yield self
-        self.skipto = None
+        self.skip_from = None
         self.cost = None
-
-    @property
-    def is_sane(self) -> bool:
-        return all(ow.is_sane for ow in self.opt_waypoints)
 
     def feed(self, visited: Waypoint) -> Iterator['Solver']:
         self.visited = visited
         self.waypoints.append(visited)
         yield self
 
-        if not NDEBUG:
-            assert visited.is_sane
-            assert self.is_sane
-
         self.total_penalty += visited.penalty
 
         yield from self.get_best_cost()
 
-        self.new_opt = OptimisedWaypoint.with_cost(waypoint=visited, best_cost=self.best_cost)
-        if not NDEBUG:
-            assert self.new_opt.is_sane
+        self.new_opt = OptimisedWaypoint.with_cost(waypoint=visited, cost_best=self.cost_best)
+        assert self.new_opt.is_sane
         yield self
 
         if self.new_opt.cost_min <= self.acceptable_cost and self.new_opt.emplace(self.opt_waypoints):
@@ -208,27 +69,28 @@ class Solver:
         self.new_opt = None
 
     def finish(self) -> Iterator['Solver']:
-        self.visited = self.HEAD
-        self.waypoints.append(self.HEAD)
+        self.visited = self.TAIL
+        self.waypoints.append(self.TAIL)
         yield from self.get_best_cost()
-        self.final_time = self.best_cost + self.total_penalty
+        self.final_time = self.cost_best + self.total_penalty
 
-    def iterate_solve(self, reader: WaypointReader) -> Iterator['Solver']:
+    def iterate_solve(self, in_: TextIO, n: int) -> Iterator['Solver']:
         yield self
 
-        while reader.advance_state():
-            yield from self.feed(reader.get_next())
+        for line in islice(in_, n):
+            visited = Waypoint.from_line(line)
+            assert visited.is_sane
+            yield from self.feed(visited)
 
-        if not reader.stream_end:
-            yield from self.finish()
+        yield from self.finish()
 
 
 class AnimateContext:
     SCALE = 8
 
-    def __init__(self, reader: WaypointReader) -> None:
+    def __init__(self, in_: TextIO, n: int) -> None:
         self.solver = Solver()
-        self.steps: Iterator[Solver] = chain(self.solver.iterate_solve(reader), self.solver.finish())
+        self.steps: Iterator[Solver] = chain(self.solver.iterate_solve(in_, n), self.solver.finish())
 
         w = self.SCALE*(EDGE + 1)
         self.tk = tk.Tk()
@@ -269,8 +131,8 @@ class AnimateContext:
 
     def create_waypoints(self, step: Solver) -> None:
         for waypoint in (
+            step.HEAD.waypoint,
             step.visited,
-            step.TAIL.waypoint,
         ):
             if waypoint is not None and waypoint not in self.waypoint_oval_ids:
                 penalty = waypoint.penalty
@@ -293,7 +155,7 @@ class AnimateContext:
                 ow.waypoint for ow in step.opt_waypoints[step.prune_from:]
             }:
                 fill = '#952d1a'  # dark red
-            elif step.skipto and waypoint == step.skipto.waypoint:
+            elif step.skip_from and waypoint == step.skip_from.waypoint:
                 fill = '#93cadb'  # light blue
             elif waypoint == step.opt_waypoints[0].waypoint:
                 fill = '#8916a6'  # purple
@@ -327,7 +189,7 @@ class AnimateContext:
 
         # cost = distance/speed + best_cost - penalty + delay
         # (acceptable_cost - invariant)*speed = reachable distance
-        reachable_dist = (step.acceptable_cost - target.invariant_cost) * SPEED
+        reachable_dist = (step.acceptable_cost - target.cost_invariant) * SPEED
         radius = max(5., reachable_dist*self.SCALE)
 
         if step.prune_from or step.acceptable_cost < target.cost_min:
@@ -362,9 +224,9 @@ class AnimateContext:
             source = step.opt_waypoints[0].waypoint
             target = step.opt_waypoints[-1].waypoint
             outline = '#d33a23'  # red
-        elif step.skipto:
+        elif step.skip_from:
             source = step.visited
-            target = step.skipto.waypoint
+            target = step.skip_from.waypoint
             outline = '#93cadb'  # light blue
         elif step.new_opt:
             source = step.opt_waypoints[0].waypoint
@@ -387,16 +249,14 @@ class AnimateContext:
 
 
 def process_stream(in_: TextIO, out: TextIO) -> None:
-    reader = WaypointReader(in_)
+    while True:
+        n = int(next(in_))
+        if n == 0:
+            break
 
-    times = []
-
-    while not reader.stream_end:
-        animate = AnimateContext(reader)
+        animate = AnimateContext(in_, n)
         animate.run()
-        times.append(animate.finish())
-
-    for time in times[::-1]:
+        time = animate.finish()
         out.write(f'{time:.3f}\n')
 
 
